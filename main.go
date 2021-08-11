4df89093ad50b80910cd7bfa1147157491e87dd2
Gusyatnikova <crysnat08@gmail.com>
2021-08-11 18:19:13 +0500

    add timer to quiz

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
+	"time"
 )
 
 func ParseCSV(fileName string) [][]string {
@@ -15,6 +17,10 @@ func ParseCSV(fileName string) [][]string {
 	}
 	csvReader := csv.NewReader(file)
 	csvContent, readerErr := csvReader.ReadAll()
+	for _, problem := range csvContent {
+		problem[0] = strings.TrimSpace(problem[0])
+		problem[1] = strings.TrimSpace(problem[1])
+	}
 	if readerErr != nil {
 		log.Fatal("Cannot parse file "+fileName+" as CSV", readerErr)
 	}
@@ -26,22 +32,37 @@ func ParseCSV(fileName string) [][]string {
 	return csvContent
 }
 
+func askInput() {
+	fmt.Print("Press Enter to start quiz.")
+	fmt.Scanln()
+}
+
 func main() {
-	//todo: add a timer based on timeLimit flag:
 	quizFile := flag.String("csv", "problems.csv",
 		"a csv file in the format of \"question, answer\" (default \"problems.csv\"")
-	timeLimit := flag.Int("limit", 10, "the time limit for the quiz in seconds (default 10)")
+	timeLimit := flag.Int("limit", 30, "the time limit for the quiz in seconds (default 30)")
 	flag.Parse()
-	var correct uint
-	fmt.Println("Time limit for this quiz = ", *timeLimit)
 	quiz := ParseCSV(*quizFile)
+	askInput()
+	timer := time.NewTimer(time.Duration(*timeLimit) * time.Second)
+	var correct uint
 	for i, curLine := range quiz {
-		var userAnswer string
 		fmt.Print("Problem #", i+1, ": ", curLine[0], " = ")
-		fmt.Scanf("%s\n", &userAnswer)
-		if userAnswer == curLine[1] {
-			correct++
+		answerCh := make(chan string)
+		go func() {
+			var userAnswer string
+			fmt.Scanf("%s\n", &userAnswer)
+			answerCh <- userAnswer
+		}()
+		select {
+		case <-timer.C:
+			fmt.Printf("\nTime is up. You scored %d out of %d.\n", correct, len(quiz))
+			return
+		case answer := <-answerCh:
+			if answer == curLine[1] {
+				correct++
+			}
 		}
 	}
-	fmt.Println("You scored ", correct, " out of ", len(quiz), ".")
+	fmt.Printf("You scored %d out of %d.\n", correct, len(quiz))
 }
